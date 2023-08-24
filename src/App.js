62251531d7e937c4f1715a197b0452a0336dffb1
Mike Harper <mike.harper@agoradcm.com>
2023-08-24 18:06:55 +0100

    Fix warnings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import { useQuery, gql } from '@apollo/client';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
