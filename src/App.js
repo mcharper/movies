b97b7f608fd904d3dc45806aa7cd6a891a8007a9
Mike Harper <mike.harper@agoradcm.com>
2023-08-24 17:58:50 +0100

    Change to use another API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+const GET_MOVIES = gql`
+query GetMovies {
+	discover {
+		    movies {
+          popular {
+            edges {
+              node {
+                title
+                overview
+                poster(size: W342)
+              }
+            }
+          }
+        }
+  }
+}`;
+
+function DisplayMovies() {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+  console.log(data);
+
+  return data.discover.movies.popular.edges.map(({ node }) => (
+    <Card style={{ width: '20rem', marginBottom: '5rem' }}>
+      <Card.Img variant="top" src={`${node.poster}`} />
+      <Card.Body>
+        <Card.Title><h2>{node.title}</h2></Card.Title>
+        <Card.Text>
+          {node.overview}
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  ));
+}
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h2>Movies</h2>
+      <Container fluid>
+        <DisplayMovies />
+      </Container>
+
+    </div >
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  // uri: 'https://flyby-router-demo.herokuapp.com/',
+  uri: 'https://tmdb.apps.quintero.io/',
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
   </React.StrictMode>
 );
 
