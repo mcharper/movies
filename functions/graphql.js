058390199832013326237bc952ee95ff5038b211
Mike Harper <mike.harper@agoradcm.com>
2023-08-24 19:22:39 +0100

    Changes

---
+++ functions/graphql.js
@@ -0,0 +1,30 @@
+const { ApolloServer, gql } = require("apollo-server-lambda");
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: (parent, args, context) => {
+      return "Hello, world!";
+    }
+  }
+};
+
+const getHandler = (event, context) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    debug: true,
+  });
+  const graphqlHandler = server.createHandler();
+  if (!event.requestContext) {
+    event.requestContext = context;
+  }
+  return graphqlHandler(event, context);
+}
+
+exports.handler = getHandler;
